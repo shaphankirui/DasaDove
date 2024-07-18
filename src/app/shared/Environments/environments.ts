1793eb97ac122aa9380985e4efc815b695c5bcc8
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-07-18 16:14:18 +0300

    api

--- src/app/shared/Environments/environments.ts
+++ src/app/shared/Environments/environments.ts
@@ -2,8 +2,8 @@ import { EnvironmentInterface } from '../interfaces/environments.interface';
 
 // API URL FOR PRODUCTION
 const apiLocalHostURL = 'https://cpos-shop-backend.vercel.app/';
-const apiProductionURL = ' http://localhost:3000/';
-// const apiProductionURL = ' https://dasa-dove-backend.vercel.app/';
+// const apiProductionURL = ' http://localhost:3000/';
+const apiProductionURL = ' https://dasa-dove-backend.vercel.app/';
 // export const apiProductionURL = 'https://api.c-pos.co.ke/api';
 
 export const environment: EnvironmentInterface = {
